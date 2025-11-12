d7c397bd20d11174330d1b189c2b8496d1d074be
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 14:37:51 -0500

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -1,28 +1,29 @@
-import streamlit as st
+# import streamlit as st
+######
+## option 1
+# a, b = st.columns([1, 1.6 ])
 
-
-a, b = st.columns([1, 1.6 ])
-
-a.write("")
-a.write("")
-a.write("")
-a.write("")
-a.write("")
-a.write("")
-a.subheader("Please contact:")
-a.write("")
-a.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
-a.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
-# b.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
-# b.markdown("""<span style="font-size:18px;">University of Pittsburgh, UPMC Hillman Cancer Center, Assembly Building</span>""", unsafe_allow_html=True)
-b.markdown("""<div style="position:relative;padding-bottom:100%;">
-                <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;"
-                frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" 
-               src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5067%20CBaum%20Blvd%20Pittsburgh,%20PA%2015206+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                <a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+# a.write("")
+# a.write("")
+# a.write("")
+# a.write("")
+# a.write("")
+# a.write("")
+# a.subheader("Please contact:")
+# a.write("")
+# a.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
+# a.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
+# # b.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
+# # b.markdown("""<span style="font-size:18px;">University of Pittsburgh, UPMC Hillman Cancer Center, Assembly Building</span>""", unsafe_allow_html=True)
+# b.markdown("""<div style="position:relative;padding-bottom:100%;">
+#                 <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;"
+#                 frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" 
+#                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5067%20CBaum%20Blvd%20Pittsburgh,%20PA%2015206+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+#                 <a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
     
 
-
+##########
+# option 2, needs to Add the Secret based on the hosting service, AI tool can give the soultion
 
 
 # st.markdown(
@@ -141,6 +142,10 @@ b.markdown("""<div style="position:relative;padding-bottom:100%;">
 #         else:
 #             st.error(f"❌ Error sending email (code {status})")
 
+
+##########
+# option 3
+# use https://formspree.io/ free plan
 import streamlit as st
 import requests
 
