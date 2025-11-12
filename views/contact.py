4ded25a2db366995d740289ad0f4320861db7890
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 12:57:39 -0500

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -29,7 +29,7 @@
 import streamlit as st
 
 # page config
-st.set_page_config(page_title="Contact Us", layout="centered")
+# st.set_page_config(page_title="Contact Us", layout="centered")
 
 # some custom CSS for styling
 st.markdown(
