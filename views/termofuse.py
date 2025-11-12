e173631a844f0f831fc400149158733a05de4f7f
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 12:41:28 -0500

    Update termofuse.py

--- views/termofuse.py
+++ views/termofuse.py
@@ -1,26 +1,5 @@
 import streamlit as st
 
-st.subheader("DGAT Web Server – Terms of Use")
-st.markdown("<p>By using the <strong>DGAT (Dual-Graph Attention Network)</strong> web server, you agree to the following:</p>", unsafe_allow_html=True)
-
-
-st.markdown("""
-• **Research Only:** DGAT is provided for non-commercial, academic research use.  
-• **At Your Own Risk:** Use is at your own risk; the developers and affiliated institutions are not liable for any loss or damage.  
-• **Feedback Welcome:** You may share feedback to help us improve DGAT.  
-• **License:** Results are released under **CC BY-NC 4.0**.  
-• **Citation:** Please cite: 
-
-Haoyu Wang, Brittany A Cody, Hatice Ulku Osmanbeyoglu.
-<br>
-DGAT: A Dual-Graph Attention Network for Inferring Spatial Protein Landscapes from Transcriptomics.
-<br>
-bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121
-
-
-""", unsafe_allow_html=True)
-
-
 st.markdown("""
 # <div style="border:1px solid #ccc; padding:1em; border-radius:8px; background-color:#fafafa;">
 <h4>DGAT Web Server – Terms of Use <b>DGAT</b></h4>
@@ -32,9 +11,9 @@ st.markdown("""
 <li><b>Feedback Welcome:</b> You may share feedback to help us improve DGAT.</li>
 <li><b>License:</b> Results are released under <b>CC BY-NC 4.0</b>.</li>
 <li><b>Citation:</b> Please cite</li>
-<p style="background-color:#fff59d;"><i>Haoyu Wang, Brittany A Cody, Hatice Ulku Osmanbeyoglu.</i>  “DGAT: A Dual-Graph Attention Network for Spatially Resolved Protein Imputation from Transcriptomic Data,” <i>bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121.</p>
+<p ><i>Haoyu Wang, Brittany A Cody, Hatice Ulku Osmanbeyoglu.</i>  “DGAT: A Dual-Graph Attention Network for Spatially Resolved Protein Imputation from Transcriptomic Data,” <i>bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121.</p>
 <li><b>Commercial Use:</b> Contact the DGAT team for licensing options.</li>
 </ul>
-# </div>
+</div>
 """, unsafe_allow_html=True)
 
