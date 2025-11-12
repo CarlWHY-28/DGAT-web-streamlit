6b4fd68a06a13db7d62d50eb91d9c8327a277fd3
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 12:39:47 -0500

    Update termofuse.py

--- views/termofuse.py
+++ views/termofuse.py
@@ -24,6 +24,8 @@ bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121
 st.markdown("""
 # <div style="border:1px solid #ccc; padding:1em; border-radius:8px; background-color:#fafafa;">
 <h4>DGAT Web Server – Terms of Use <b>DGAT</b></h4>
+st.markdown("<p>By using the <strong>DGAT (Dual-Graph Attention Network)</strong> web server, you agree to the following:</p>", unsafe_allow_html=True)
+
 <ul>
 <li><b>Research Only:</b> DGAT is provided for non-commercial, academic research use.</li>
 <li><b>At Your Own Risk:</b> Use is at your own risk; the developers and affiliated institutions are not liable for any loss or damage.</li>
