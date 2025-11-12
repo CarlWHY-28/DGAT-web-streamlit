fa797a250ed7e226c0134b01a4ec0b8e5c14a743
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 12:53:45 -0500

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -15,8 +15,13 @@ a.write("")
 a.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
 a.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
 b.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
-b.markdown("""<span style="font-size:18px;">University of Pittsburgh, UPMC Hillman Cancer Center, Assembly Building</span>""", unsafe_allow_html=True)
-
+# b.markdown("""<span style="font-size:18px;">University of Pittsburgh, UPMC Hillman Cancer Center, Assembly Building</span>""", unsafe_allow_html=True)
+b.markdown("""<div style="position:relative;padding-bottom:100%;">
+                <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;"
+                frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" 
+               src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                <a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+    
 st.markdown("#")
 
 
