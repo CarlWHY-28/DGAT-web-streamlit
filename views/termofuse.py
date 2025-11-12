cdd01a7f1603f0bda38c37b6cae32d455a46cef2
osmanlab2025 <osmanbeyoglulab@outlook.com>
2025-11-12 12:37:41 -0500

    Update termofuse.py

--- views/termofuse.py
+++ views/termofuse.py
@@ -16,5 +16,23 @@ Haoyu Wang, Brittany A Cody, Hatice Ulku Osmanbeyoglu.
 DGAT: A Dual-Graph Attention Network for Inferring Spatial Protein Landscapes from Transcriptomics.
 <br>
 bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121
+
+
+""", unsafe_allow_html=True)
+
+
+st.markdown("""
+<div style="border:1px solid #ccc; padding:1em; border-radius:8px; background-color:#fafafa;">
+<h4>⚠️ Terms of Use for <b>DGAT</b></h4>
+<ul>
+<li><b>Research Only:</b> DGAT is provided for non-commercial, academic research use.</li>
+<li><b>At Your Own Risk:</b> Use is at your own risk; the developers and affiliated institutions are not liable for any loss or damage.</li>
+<li><b>Feedback Welcome:</b> You may share feedback to help us improve DGAT.</li>
+<li><b>License:</b> Results are released under <b>CC BY-NC 4.0</b>.</li>
+<li><b>Citation:</b> Please cite</li>
+<p style="background-color:#fff59d;"><i>Haoyu Wang, Brittany A Cody, Hatice Ulku Osmanbeyoglu.</i>  “DGAT: A Dual-Graph Attention Network for Spatially Resolved Protein Imputation from Transcriptomic Data,” <i>bioRxiv 2025.07.05.662121; doi: https://doi.org/10.1101/2025.07.05.662121.</p>
+<li><b>Commercial Use:</b> Contact the DGAT team for licensing options.</li>
+</ul>
+</div>
 """, unsafe_allow_html=True)
 
