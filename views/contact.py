e65a3fb6ea7e52d8128edf19bc4600c7af8e845f
xim3320 <63321472+xim3320@users.noreply.github.com>
2025-11-13 18:19:42 -0500

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
-a, b = st.columns([1, 1.6 ])
+a, b, c, d = st.columns([3, 1, 1, 1 ])
 
 a.write("")
 a.write("")
-a.write("")
-a.write("")
+
 
 a.subheader("Please contact:")
 a.write("")
-a.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
-a.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
-# b.image('logo/dbmi.png', use_column_width=True)
-b.markdown("<p style='text-align: center;'>University of Pittsburgh<br>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
-# b.markdown("<p style='text-align: center;'>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
+c.image('logo/dbmi.png', use_column_width=True)
 
+a2, b2 = st.columns([3,3])
+a2.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
+a2.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
+
+b2.markdown("<p style='text-align: center;'>University of Pittsburgh<br>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
+# b.markdown("<p style='text-align: center;'>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
+1
 # b.markdown("""<div style="position:relative;padding-bottom:100%;">
 #                 <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;"
 #                 frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" 
