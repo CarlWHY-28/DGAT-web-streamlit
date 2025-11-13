dc4fa82b2a27a80bc1b98866094a77e843a72900
xim3320 <63321472+xim3320@users.noreply.github.com>
2025-11-13 18:16:12 -0500

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -12,8 +12,8 @@ a.write("")
 a.markdown("""<span style="font-size:16px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
 a.markdown("""<span style="font-size:16px;">Haoyu Wang<br>PhD Student<br>✉️ haw309@pitt.edu</span>""", unsafe_allow_html=True)
 # b.image('logo/dbmi.png', use_column_width=True)
-b.markdown("<p style='text-align: center;'>University of Pittsburgh</p>", unsafe_allow_html=True)
-b.markdown("<p style='text-align: center;'>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
+b.markdown("<p style='text-align: center;'>University of Pittsburgh<br>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
+# b.markdown("<p style='text-align: center;'>Department of Biomedical Informatics</p>", unsafe_allow_html=True)
 
 # b.markdown("""<div style="position:relative;padding-bottom:100%;">
 #                 <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;"
